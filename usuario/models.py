6686c0fa8eb6e4bf643cdff15125c3e6293842ab
Anailton Silva <francisco.anailton17@gmail.com>
2023-09-29 19:53:02 -0300

    finalizando as models para começar as validadoções

--- usuario/models.py
+++ usuario/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Usuario(models.Model):
+class Cadastro(models.Model):
     nome = models.CharField(max_length=30)
     email = models.EmailField()
-    login = models.CharField(max_length=30, unique=True)
+    login = models.CharField(max_length=30)
     senha = models.CharField(max_length=64)
 
     def __str__(self):
--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from usuario.models import Usuario
+from django.shortcuts import render, redirect
+from .models import Cadastro
 
 
 def login(request):
@@ -15,6 +14,14 @@ def validação(request):
     email = request.POST.get('email')
     nome = request.POST.get('nome')
     senha = request.POST.get('senha')
+    conf_senha = request.POST.get('conf_senha')
+    usuario = Cadastro(email=email,
+                       nome=nome,
+                       senha=senha)
+
+    usuario.save()
+
+    return redirect('/cadastrar/?status=0')
 
 
 def recuperar(request):
